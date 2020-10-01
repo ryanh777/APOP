61a0f206d0f8015ac04c230db9d88375362b8bba
ryanh777 <49914662+ryanh777@users.noreply.github.com>
2020-10-01 11:41:39 -0400

    Testing survey elements

--- app/src/main/java/org/petobesityprevention/app/android/MainActivity.java
+++ app/src/main/java/org/petobesityprevention/app/android/MainActivity.java
@@ -2,7 +2,10 @@ package org.petobesityprevention.app.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitButton = findViewById(R.id.id_submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent surveyActivity = new Intent(getApplicationContext(), SurveyActivity.class);
+                startActivity(surveyActivity);
+            }
+        });
     }
 }
---
+++ app/src/main/java/org/petobesityprevention/app/android/SurveyActivity.java
@@ -0,0 +1,36 @@
+package org.petobesityprevention.app.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class SurveyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey);
+
+        Spinner spinner = findViewById(R.id.id_testSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.test_dropdown, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView title = findViewById(R.id.id_title);
+                title.setText(adapterView.getItemAtPosition(i).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
